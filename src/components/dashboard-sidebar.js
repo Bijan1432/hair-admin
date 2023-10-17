6f3cefb280cccd061b5dca6561547273d764cb10
Bijan1432 <bijanmondal1432@gmail.com>
2023-10-17 15:24:15 +0530

    main

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -120,7 +120,7 @@ export const DashboardSidebar = (props) => {
               }}
             >
               <div>
-                <Typography color="inherit" variant="subtitle1">
+                <Typography color="neutral.400" variant="subtitle1">
                   Hair App
                 </Typography>
                 <Typography color="neutral.400" variant="body2">
